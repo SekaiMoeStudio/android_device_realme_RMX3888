22241983908dd8904e1c76b39e00be295d86fb4e
chandu078 <chandudyavanapelli03@gmail.com>
2024-04-14 09:40:31 +0530

    sm8650-common: init: Move back kernel scripts to vendor tree
    This reverts commit d709c1b35fd63d184a8ac22a446c40b76e048f6f and 49bb9d8fd036494170c439148d48c20d40cb9f3e.
    
    Change-Id: Id2b6e28aeb8f9f98ce1f9e2b47fbe8227b80b266
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -175,8 +175,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
-    init.kernel.post_boot.sh \
-    init.kernel.post_boot-pineapple.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -184,7 +182,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
-    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
