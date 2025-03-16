1f35283dd04acfbc50f4f77f99f6205aae61d17b
LucasBlackLu <lucasblack997@gmail.com>
2025-03-16 18:43:40 +0800

    sm8650-common: Add vendorsetup script to clone denpencies

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+rm -rf hardware/qcom-caf/sm8650/audio/agm
+git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_agm hardware/qcom-caf/sm8650/audio/agm
+rm -rf hardware/qcom-caf/sm8650/audio/pal
+git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_arpal-lx hardware/qcom-caf/sm8650/audio/pal
+rm -rf hardware/qcom-caf/sm8650/audio/primary-hal
+git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_audio-ar hardware/qcom-caf/sm8650/audio/primary-hal
+rm -rf hardware/qcom-caf/sm8650/dataipa
+git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_dataipa hardware/qcom-caf/sm8650/dataipa
+rm -rf hardware/qcom-caf/sm8650/data-ipa-cfg-mgr
+git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_data-ipa-cfg-mgr hardware/qcom-caf/sm8650/data-ipa-cfg-mgr
+rm -rf hardware/qcom-caf/sm8650/display
+git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_display hardware/qcom-caf/sm8650/display
+rm -rf hardware/qcom-caf/sm8650/media
+git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_media hardware/qcom-caf/sm8650/media
+rm -rf device/qcom/sepolicy_vndr/sm8650
+git clone https://github.com/OnePlus-12-Development/android_device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr/sm8650
+rm -rf hardware/oplus
+git clone https://github.com/OnePlus-12-Development/android_hardware_oplus/ -b lineage-22.2-300325 hardware/oplus
