f6ce22a6b0c0b713c9ce22fc23473838a18a3571
LuK1337 <priv.luk@gmail.com>
2025-04-05 22:18:31 +0200

    sm8650-common: libwfdservice_shim_v1 -> libwfdservice_shim
    Change-Id: Idf65dd61a2de80dfcbf72a8b75b6ca59234df573
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -107,7 +107,7 @@ blob_fixups: blob_fixups_user_type = {
     ('odm/lib64/vendor.oplus.hardware.virtual_device.camera.manager@1.0-impl.so', 'vendor/lib64/libcwb_qcom_aidl.so'): blob_fixup()
         .add_needed('libui_shim.so'),
     'system_ext/bin/wfdservice64': blob_fixup()
-        .add_needed('libwfdservice_shim_v1.so'),
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
