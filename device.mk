fe8c7bef1bbb2d6f1ac89cb40cb7aa947f0ae3e2
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2025-07-03 16:48:02 +0000

    tmp
    Change-Id: I15f1514bd27fe71c5852f420e80747a7f8fe0125

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ PRODUCT_PACKAGES += \
     libagm_pcm_plugin \
     libaudiochargerlistener \
     libbatterylistener \
+    libats \
+    libcustomva_intf \
+    libhotword_intf \
     libfmpal \
     libhfp_pal \
     libqcompostprocbundle \
@@ -91,7 +94,8 @@ PRODUCT_PACKAGES += \
     libsndcardparser \
     libvolumelistener \
     sound_trigger.primary.pineapple \
-    vendor.qti.audio-adsprpc-service.rc
+    vendor.qti.audio-adsprpc-service.rc \
+    vendor.qti.hardware.AGMIPC@1.0-impl
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8650/audio/primary-hal
 AUDIO_PAL_DIR := hardware/qcom-caf/sm8650/audio/pal
@@ -117,6 +121,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
 
 # Bluetooth
+PRODUCT_PACKAGES += \
+    lib_bt_aptx \
+    lib_bt_ble \
+    lib_bt_bundle
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
