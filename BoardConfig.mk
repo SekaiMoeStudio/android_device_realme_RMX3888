4ad5a0a09ad69829fe07877bdc696c16769f5e17
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2025-07-05 14:03:56 +0000

    RMX388: Drop debugfs kernel config
    Change-Id: I8a1416365572a6a370097584c2aa5737721300c8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,8 +140,7 @@ TARGET_KERNEL_SOURCE := kernel/realme/sm8650
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/pineapple_GKI.config \
-    vendor/oplus/pineapple_GKI.config \
-    vendor/debugfs.config
+    vendor/oplus/pineapple_GKI.config
 
 # Kernel modules
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.system_dlkm))
@@ -155,30 +154,35 @@ BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/realme/sm8650-modules
 TARGET_KERNEL_EXT_MODULES := \
-	qcom/opensource/mmrm-driver \
-	qcom/opensource/mm-drivers/hw_fence \
-	qcom/opensource/mm-drivers/msm_ext_display \
-	qcom/opensource/mm-drivers/sync_fence \
-	qcom/opensource/audio-kernel \
-	qcom/opensource/camera-kernel \
-	qcom/opensource/dataipa/drivers/platform/msm \
-	qcom/opensource/datarmnet/core \
-	qcom/opensource/datarmnet-ext/aps \
-	qcom/opensource/datarmnet-ext/offload \
-	qcom/opensource/datarmnet-ext/shs \
-	qcom/opensource/datarmnet-ext/perf \
-	qcom/opensource/datarmnet-ext/perf_tether \
-	qcom/opensource/datarmnet-ext/sch \
-	qcom/opensource/datarmnet-ext/wlan \
-	qcom/opensource/securemsm-kernel \
-	qcom/opensource/display-drivers/msm \
-	qcom/opensource/eva-kernel \
-	qcom/opensource/video-driver \
-	qcom/opensource/graphics-kernel \
-	qcom/opensource/wlan/platform \
-	qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
-	qcom/opensource/bt-kernel \
-	nxp/opensource/driver
+ 	qcom/opensource/mmrm-driver \
+ 	qcom/opensource/mm-drivers/hw_fence \
+ 	qcom/opensource/mm-drivers/msm_ext_display \
+ 	qcom/opensource/mm-drivers/sync_fence \
+ 	qcom/opensource/securemsm-kernel \
+ 	qcom/opensource/audio-kernel \
+ 	qcom/opensource/synx-kernel \
+ 	qcom/opensource/camera-kernel \
+ 	qcom/opensource/datarmnet-ext/mem \
+ 	qcom/opensource/dataipa/drivers/platform/msm \
+ 	qcom/opensource/datarmnet/core \
+ 	qcom/opensource/datarmnet-ext/aps \
+ 	qcom/opensource/datarmnet-ext/offload \
+ 	qcom/opensource/datarmnet-ext/shs \
+ 	qcom/opensource/datarmnet-ext/perf \
+ 	qcom/opensource/datarmnet-ext/perf_tether \
+ 	qcom/opensource/datarmnet-ext/sch \
+ 	qcom/opensource/datarmnet-ext/wlan \
+ 	qcom/opensource/display-drivers/msm \
+ 	qcom/opensource/dsp-kernel \
+ 	qcom/opensource/eva-kernel \
+ 	qcom/opensource/video-driver \
+ 	qcom/opensource/graphics-kernel \
+ 	qcom/opensource/wlan/platform \
+ 	qcom/opensource/wlan/qcacld-3.0 \
+ 	qcom/opensource/bt-kernel \
+ 	qcom/opensource/spu-kernel \
+ 	qcom/opensource/mm-sys-kernel/ubwcp \
+ 	nxp/opensource/driver
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
